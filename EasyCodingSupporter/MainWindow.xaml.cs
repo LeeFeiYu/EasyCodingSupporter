6b4addbe63b54e7b199a61f950b599e04904be5e
Fei Li <mighty-knight@hanmail.net>
2020-10-11 21:19:08 +0900

    클래스 생성

--- EasyCodingSupporter/MainWindow.xaml.cs
+++ EasyCodingSupporter/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EasyCodingSupporter.Class;
 
 namespace EasyCodingSupporter
 {
@@ -23,6 +25,24 @@ namespace EasyCodingSupporter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Run버튼을 눌렀을 때 MainTextBox에서 문자를 읽어오고 문자를 분석하여 로드된 파일에서 값들을 대입함.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRun_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(tbxMain.Text) == false)
+            {
+                LoadFileClass Coding = new LoadFileClass();
+                RunCodingClass File = new RunCodingClass();
+            }
+            else
+            {
+                MessageBox.Show("텍스트 박스에 내용이 없습니다", "입력에러");
+            }
         }
     }
 }
